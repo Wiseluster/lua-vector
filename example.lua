e78995c4d14a0b46f3dfaf3b89d8d072b8f89a6d
Wiseluster <chinaware@foxmail.com>
2016-09-28 22:52:39 +0800

    Update sources

---
+++ example.lua
@@ -0,0 +1,18 @@
+#!/usr/bin/env lua
+
+require("lua-vector"); -- Loads the module "lua-vector"
+
+local a = vector(3, 4, 2);
+local b = vector(5, -1, 0.5);
+
+print(a);
+print(b);
+print(a + b); -- Prints the sum of a and b
+print(a - b); -- Prints the difference of a and b
+print(a * b); -- Prints the scalar product of a and b
+print(a * 2); -- Prints a times 2
+print(3 * a); -- Prints a times 3
+print(a ^ b); -- Prints the vector product of a and b
+print(-a); -- Prints the opposite vector of a
+print(a == b); -- Prints the boolean value of the proposition "a equals b"
+print(#a); -- Prints the length of a
